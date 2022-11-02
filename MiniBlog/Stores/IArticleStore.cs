47bd5666f002c0ebc554d729ee7e78d8974361a2
Bo Zhao <BZhao7@slb.com>
2022-11-02 15:40:19 +0800

    refactor: Add DI

---
+++ MiniBlog/Stores/IArticleStore.cs
@@ -0,0 +1,13 @@
+﻿using MiniBlog.Model;
+
+namespace MiniBlog.Stores
+{
+    public interface IArticleStore
+    {
+        public Article Save(Article article);
+
+        public List<Article> GetAll();
+
+        public bool Delete(Article articles);
+    }
+}
