1fc0a9effbcbb6226bd1ecaf35f2b96e260fb347
Jiahui Xiong <jxiong5@slb.com>
2022-11-02 16:08:46 +0800

    feat: replace ArticleStoreWillReplaceInFuture of UserController and fix test of UserControllerText

---
+++ MiniBlog/Stores/IUserStore.cs
@@ -0,0 +1,12 @@
+﻿using MiniBlog.Model;
+
+namespace MiniBlog.Stores
+{
+    public interface IUserStore
+    {
+        bool Delete(User user);
+        List<User> GetAll();
+        void Init();
+        User Save(User user);
+    }
+}
---
+++ MiniBlog/Stores/UserStoreContext.cs
@@ -0,0 +1,27 @@
+﻿using MiniBlog.Model;
+
+namespace MiniBlog.Stores
+{
+    public class UserStoreContext : IUserStore
+    {
+        public bool Delete(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Save(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
